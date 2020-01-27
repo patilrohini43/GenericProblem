b0aec51ec995783ac43302f8c61be25d84012c57
rohini <patilrohini43@gmail.com>
2020-01-27 16:58:17 +0530

    Refactord:three test cases calling generics

--- src/test/java/MaxNumberTest.java
+++ src/test/java/MaxNumberTest.java
@@ -4,4 +4,17 @@ import org.junit.Test;
 
 public class MaxNumberTest {
 
+    @Test
+    public void givenIntegerNumber() {
+        Assert.assertEquals(6,MaxNumber.maxNumber(4,2,6),00);
+    }
+    @Test
+    public void givenFloatNumber() {
+        Assert.assertEquals(4.2f, MaxNumber.maxNumber(4.2f, 2.4f, 4.0f),00);
+    }
+
+    @Test
+    public void givenString(){
+        Assert.assertEquals("0","Peach",MaxNumber.maxNumber("Apple","Peach","Banana"));
+    }
 }
