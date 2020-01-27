f307029c7d434086762a7e4531e79e7e6c1dc0d2
rohini <patilrohini43@gmail.com>
2020-01-27 15:39:41 +0530

    Added:three string number and finding the maxStringNumber

--- src/main/java/com/bridgelabz/com/MaxNumber.java
+++ src/main/java/com/bridgelabz/com/MaxNumber.java
@@ -25,4 +25,15 @@ public class MaxNumber {
         }
         return max;
     }
+
+    public static String findStringMaxNumber(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 }
--- src/test/java/MaxNumberTest.java
+++ src/test/java/MaxNumberTest.java
@@ -6,40 +6,57 @@ public class MaxNumberTest {
 
     @Test
     public void givenMaxNumber_atFirstPosition_then_returnSameNumber() {
-        int maxNumber=MaxNumber.findMaxNumber(10,5,2);
-        Assert.assertEquals(10,maxNumber);
+        int maxInteger=MaxNumber.findMaxNumber(10,5,2);
+        Assert.assertEquals(10,maxInteger);
     }
 
     @Test
     public void givenMaxNumber_atSecondPosition_then_returnSameNumber() {
-        int maxNumber=MaxNumber.findMaxNumber(10,15,2);
-        Assert.assertEquals(15,maxNumber);
+        int maxInteger=MaxNumber.findMaxNumber(10,15,2);
+        Assert.assertEquals(15,maxInteger);
     }
 
     @Test
     public void givenMaxNumber_atThirdPosition_then_returnSameNumber() {
-        int maxNumber=MaxNumber.findMaxNumber(10,15,20);
-        Assert.assertEquals(20,maxNumber);
+        int maxInteger=MaxNumber.findMaxNumber(10,15,20);
+        Assert.assertEquals(20,maxInteger);
     }
 
     @Test
     public void givenFloatMaxNumber_atFirstPosition_then_returnSameNumber() {
-        float maxNumber=MaxNumber.findFloatMaxNumber(7.1f,2.5f,4.6f);
-        Assert.assertEquals(7.1f,maxNumber,00);
+        float maxFloat=MaxNumber.findFloatMaxNumber(7.1f,2.5f,4.6f);
+        Assert.assertEquals(7.1f,maxFloat,00);
     }
 
     @Test
     public void givenFloatMaxNumber_atSecondPosition_then_returnSameNumber() {
-        float maxNumber=MaxNumber.findFloatMaxNumber(7.1f,8.5f,4.6f);
-        Assert.assertEquals(8.5f,maxNumber,00);
+        float maxFloat=MaxNumber.findFloatMaxNumber(7.1f,8.5f,4.6f);
+        Assert.assertEquals(8.5f,maxFloat,00);
     }
 
     @Test
     public void givenFloatMaxNumber_atThirdPosition_then_returnSameNumber() {
-        float maxNumber=MaxNumber.findFloatMaxNumber(1.1f,2.5f,4.6f);
-      Assert.assertEquals(4.6f,maxNumber,00);
+        float maxFloat=MaxNumber.findFloatMaxNumber(1.1f,2.5f,4.6f);
+      Assert.assertEquals(4.6f,maxFloat,00);
     }
 
+    @Test
+    public void givenStringMaxNumber_atFirsPosition_then_returnSameNumber() {
+        String maxString=MaxNumber.findStringMaxNumber("Peach","Apple","Banana");
+        Assert.assertEquals("Peach",maxString);
+    }
+
+    @Test
+    public void givenStringMaxNumber_atSecondPosition_then_returnSameNumber() {
+        String maxString=MaxNumber.findStringMaxNumber("Apple","Peach","Banana");
+        Assert.assertEquals("Peach",maxString);
+    }
+
+    @Test
+    public void givenStringMaxNumber_atThirdPosition_then_returnSameNumber() {
+        String maxString=MaxNumber.findStringMaxNumber("Apple","Banana","Peach");
+        Assert.assertEquals("Peach",maxString);
+    }
 
 
 }
